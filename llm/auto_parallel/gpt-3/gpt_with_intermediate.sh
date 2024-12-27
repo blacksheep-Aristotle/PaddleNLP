cdcfcc6f3e3518bc68cb3d70a03677badd91c1f2
blacksheep-Aristotle <zhangweilong01@baidu.com>
2024-12-27 10:43:19 +0800

    [AutoParallel]:add explanatory note

--- llm/auto_parallel/gpt-3/gpt_with_intermediate.sh
+++ llm/auto_parallel/gpt-3/gpt_with_intermediate.sh
@@ -1,5 +1,5 @@
 # Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
-# 
+# PaddlePaddle version 3.0-beta2 or higher is required, please upgrade your PaddlePaddle to 3.0-beta2 or other higher version.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
