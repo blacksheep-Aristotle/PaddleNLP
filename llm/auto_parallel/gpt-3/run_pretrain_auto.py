f53a47221f93e0d8d1fe8bfb26abfc0b52b32938
blacksheep-Aristotle <1146009299@qq.com>
2025-01-13 17:18:20 +0800

    Update run_pretrain_auto.py

--- llm/auto_parallel/gpt-3/run_pretrain_auto.py
+++ llm/auto_parallel/gpt-3/run_pretrain_auto.py
@@ -494,7 +494,7 @@ def main():
     config.recompute_granularity = model_args.recompute_granularity
     config.virtual_pp_degree = model_args.virtual_pp_degree
     config.sequence_parallel = training_args.sequence_parallel
-
+    config.fuse_sequence_parallel_allreduce = training_args.fuse_sequence_parallel_allreduce
     config.use_fused_rope = model_args.use_fused_rope
     config.no_recompute_layers = model_args.no_recompute_layers
     config.pp_recompute_interval = model_args.pp_recompute_interval
